98e5d60d497d0148972c7596729b29aee59f3ae0
Yasminebch11 <149058771+Yasminebch11@users.noreply.github.com>
2024-04-09 11:23:03 +0200

    hh

--- lifesync/test/widget_test.dart
+++ lifesync/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:lifesync/main.dart';
+import 'package:lifesync/splash.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
